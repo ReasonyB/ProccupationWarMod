dd51c6dc11b439b0ecf63a707ae1d9a024c05039
ReasonB <ydlwnsj25@gmail.com>
2024-01-20 21:19:17 +0900

    format of java classes

---
+++ src/main/java/io/github/reasonyb/proccupationwar/ProccupationWar.java
@@ -0,0 +1,8 @@
+package io.github.reasonyb.proccupationwar;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ProccupationWar.MODID)
+public class ProccupationWar {
+  public static final String MODID = "proccupationwar";
+}
