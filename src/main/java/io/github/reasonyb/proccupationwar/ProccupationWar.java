4282c62b4718aa039d0b5dc7097528b88a7a3df4
ReasonB <ydlwnsj25@gmail.com>
2024-01-21 18:38:38 +0900

    Config Class for making config file and Test it.

--- src/main/java/io/github/reasonyb/proccupationwar/ProccupationWar.java
+++ src/main/java/io/github/reasonyb/proccupationwar/ProccupationWar.java
@@ -1,8 +1,13 @@
 package io.github.reasonyb.proccupationwar;
 
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod(ProccupationWar.MODID)
 public class ProccupationWar {
   public static final String MODID = "proccupationwar";
+  public ProccupationWar(){
+    ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.SPEC, "proccupation-client.toml");
+  }
 }
